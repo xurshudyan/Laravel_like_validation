50d8f27bf25d4bbfc971226c71c923d09f93d49f
Volodya995 <xurshudyan71@gmail.com>
2022-05-08 19:46:04 +0400

    added name field

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ $validator = new Validator($data);
 $validator->validate([
     'user_name' => 'required|alfa|min:2|max:20',
     'email' => 'required|email',
-    'password' => 'required|min:8|max:50|strong|same:password_confirm'
+    'password' => 'required|min:8|max:50|strong|same:password_confirm',
+    "name" => 'required'
 ]);
 
 print_r($validator->getErrors());
