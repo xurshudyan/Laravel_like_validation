fdb7c39a97469e50f73b06c07a04e19b0bf03570
Valodia <v.khurshudyan@digitalpome.com>
2022-05-18 16:21:20 +0400

    ggggggggggg

--- index.php
+++ index.php
@@ -16,8 +16,6 @@ $validator->validate([
     'user_name' => 'required|alfa|min:2|max:20',
     'email' => 'required|email',
     'password' => 'required|min:8|max:50|strong|same:password_confirm',
-    'password' => 'required|min:8|max:50|strong|same:password_confirm',
-    'password' => 'required|min:8|max:50|strong|same:password_confirm',
 ]);
 
 print_r($validator->getErrors());
