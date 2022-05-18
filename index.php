1706673e679b875b243d5414700b6f631f328d1d
Volodya995 <70023120+Volodya995@users.noreply.github.com>
2022-05-18 17:18:58 +0400

    Revert "Revert "duplicate validation""

--- index.php
+++ index.php
@@ -19,4 +19,11 @@ $validator->validate([
     "name" => 'required'
 ]);
 
+$validator->validate([
+    'user_name' => 'required|alfa|min:2|max:20',
+    'email' => 'required|email',
+    'password' => 'required|min:8|max:50|strong|same:password_confirm',
+    "name" => 'required'
+]);
+
 print_r($validator->getErrors());
